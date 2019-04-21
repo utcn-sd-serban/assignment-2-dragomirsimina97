0ed5b3c56abd2973441fb2b2c5911e74aa33d89e
Dragomir Simina <dragomirsimina97@gmail.com>
2019-04-21 15:36:45 +0300

    Assignment2

---
+++ src/presenter/QuestionsListPresenter.js
@@ -0,0 +1,30 @@
+import model from "../model/model";
+
+class QuestionsListPresenter {
+    onCreateQuestion() {
+        window.location.assign("#/create-question");
+    }
+
+    onViewDetails(index) {
+        window.location.assign("#/question-details/" + index);
+    }
+    onFilterQuestionsTitle()
+    {
+        model.findByTitle();
+        window.location.assign("#/filter-title-question");
+    }
+    onFilterQuestionsTag()
+    {
+        model.findByTag();
+        window.location.assign("#/filter-tag-question");
+    }
+    changeToSearch(property, value){
+      model.changeToSearch(property, value);
+
+    }
+    
+}
+
+const questionsListPresenter = new QuestionsListPresenter();
+
+export default questionsListPresenter;
